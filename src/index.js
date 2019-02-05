788c6f4875abd596ee4b19dbe39cde4c6f43a4b3
Francisco J Mejias <francisco.mejias@booker.com>
2019-02-05 04:48:15 -0500

    finished project 6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App'
+import './css/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+/*For React Router to work properly, wraping the
+entire app in a BrowserRouter component*/
+ReactDOM.render(
+<BrowserRouter>
+  <App />
+</BrowserRouter>, document.getElementById('root')
+)
