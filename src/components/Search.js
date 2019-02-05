653e6721b464700d827e98be9e0998100e51e125
Francisco J Mejias <francisco.mejias@booker.com>
2019-02-05 04:51:37 -0500

    apparently when you move files to different folder they don't get committed

---
+++ src/components/Search.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import * as BooksAPI from '../api/BooksAPI'
+import Shelf from '../components/Shelf'
+
+class Search extends Component {
+
+  state = {
+    query: '',
+    searched: []
+  }
+
+//idea from contacts app
+//todo in future: figure out how to trim 2+ spaces only!
+  updateQuery =(query) => {
+    this.setState({query: query})
+    this.searched(query);}
+
+
+  //with the query entered
+  searched = (query) => {
+      //call the search API and use the returned data
+      BooksAPI.search(query).then((searched) => {
+        //if the response is greater than 0
+        if(searched.length > 0) {
+      /*iterate thru the response and assign book to 'none' or corresponding shelf */
+          searched =  searched.map((returnedBook) => {
+            returnedBook.shelf = 'none';
+            this.props.books.forEach((bookInShelf) => {
+              if(returnedBook.id === bookInShelf.id) {
+                returnedBook.shelf = bookInShelf.shelf
+              }
+            })
+            return returnedBook
+          })
+            this.setState({searched})
+        } else {
+          this.setState({searched: []})
+        }})
+  }
+
+  render() {
+
+    const {query, searched} = this.state
+
+    return (
+      <div>
+        <div className='search-books'>
+          <div className='search-books-bar'>
+            <Link className= 'close-search' to= '/'>Close</Link>
+            <div className= 'search-books-input-wrapper'>
+              <input type= 'text'
+                placeholder= 'Search by title or author'
+                value= {query}
+                onChange= {(e) => 
+                  this.updateQuery(e.target.value)
+                }
+              />
+            </div>
+          </div>
+          <div className= 'search-books-results'>
+            <ol className= 'books-grid'>
+              {query !== '' && searched.length > 0 && (
+                <Shelf
+                  title = "Result"
+                  books={searched}
+                  onUpdateShelf={(id, shelf) => {
+                    this.props.onUpdateShelf(id, shelf)
+                  }}
+                />
+                )}
+              {query !== '' && searched.length === 0 && (
+                <Shelf
+                  title = "Books not Found"
+                  books={[]}
+                />
+              )}
+            </ol>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Search
---
+++ src/components/Shelf.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Book from './Book'
+// import PropTypes from 'prop-types';
+
+class Shelf extends React.Component {
+
+  //propTypes as used in sample contacts app!
+  // static propTypes = {
+  //   shelfTitle: PropTypes.string.isRequired,
+  //   booksArray: PropTypes.array.isRequired
+  // }
+
+  render(){
+    
+    let shelfTitle = this.props.title;
+    let booksArray = this.props.books;
+
+    return (
+
+      <div className= 'bookshelf'>
+        <h2 className= 'bookshelf-title'>{`${shelfTitle}`}</h2>
+        <div className= 'bookshelf-books'>
+          <ol className= 'books-grid'>
+          {/* books go into shelves*/}
+            {booksArray.map((book) => (
+              <Book
+                id= {book.id}
+                key= {book.id}
+                title= {book.title}
+                author= {book.authors}
+                image= {book.imageLinks.thumbnail}
+                shelf= {book.shelf}
+                onUpdateShelf= {(shelf) => 
+                  {this.props.onUpdateShelf(book.id,shelf)}
+                }
+              />
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+export default Shelf
