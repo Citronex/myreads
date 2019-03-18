06a69ff689cd94d5e5563d79d654128bb0001c2d
Francisco Mejias <fxmejias@gmail.com>
2019-03-18 16:27:51 -0400

    Updates

---
+++ precache-manifest.0d47cafc1842beff88966542d1064830.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c09e57972d97d29121b6",
+    "url": "/myreads/static/css/main.f109f942.chunk.css"
+  },
+  {
+    "revision": "c09e57972d97d29121b6",
+    "url": "/myreads/static/js/main.c09e5797.chunk.js"
+  },
+  {
+    "revision": "3738e93de025062bc27b",
+    "url": "/myreads/static/js/1.3738e93d.chunk.js"
+  },
+  {
+    "revision": "1b7e853e01d73a6cc24d",
+    "url": "/myreads/static/js/runtime~main.1b7e853e.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "2af98fff9e62db72ba7a09753d0a5bb3",
+    "url": "/myreads/index.html"
+  }
+];
