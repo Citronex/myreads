2be90d3405a3f62deda652ebfc42c6a42d9779ec
Francisco Mejias <fxmejias@gmail.com>
2019-03-18 14:40:47 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/udacity_react_myreads/precache-manifest.c5b48cf9a01ecbed8d90cadf928910d8.js"
+  "/myreads/precache-manifest.1478412153f825428c749093e73db528.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/udacity_react_myreads/index.html", {
+workbox.routing.registerNavigationRoute("/myreads/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
